7c7123e3220977517717102a11bf5ef074c04df8
Madsakra <hongjun820@gmail.com>
2024-12-31 13:52:51 +0800

    aft

--- src/routes/Public_Routes/Home.tsx
+++ src/routes/Public_Routes/Home.tsx
@@ -1,30 +1,30 @@
 import { Button } from '@/components/ui/button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faApple, faGooglePlay } from '@fortawesome/free-brands-svg-icons'; // Import the specific brand icon
-import abundance_dashboard from '../../assets/Images/abundance_dashboard.png'
-import CustomAccordion from '@/customizedComponents/CustomAccordion';
-import loginImg from '../../assets/Images/login.png'
-import registerImg from '../../assets/Images/register.png'
-import placeholder1 from '../../assets/Images/placeholder-1.png'
-import placeholder2 from '../../assets/Images/placeholder-2.png'
-import { useState } from 'react';
+import abundance_dashboard from '../../assets/Images/DISPLAY_ART.png'
 import Membershipbox from '@/customizedComponents/Membershipbox';
+import { motion } from "motion/react"
+import feature2 from '../../assets/Images/feature2.jpg'
+import feature1 from '../../assets/Images/feature-1.jpg'
+import feature3 from '../../assets/Images/feature3.jpg'
+import feature4 from '../../assets/Images/feature4.png'
+import feature5 from '../../assets/Images/feature5.jpg'
+import MotionFeature from '@/customizedComponents/motionFeature';
 
-
-
+const featuresImages = [
+  feature1,feature2,feature3,feature4,feature5
+]
 
 
 const appFeatures = [
-  {trigger:'Personalized Logging Screen',content:'Organize the app the way it suits you best. Remove or add fields to suit your therapy needs.'},
-  {trigger:'RealTime Reading Of Glucose',content:'Get instant access to your glucose levels, ensuring that you are always in control of your health.'},
-  {trigger:'Chart Your Glucose Levels',content:'Visualize your glucose data through easy-to-understand charts, helping you identify patterns and trends.'},
-  {trigger:'Predict Glucose Levels',content:'Utilize advanced algorithms to anticipate future glucose levels, allowing you to proactively manage your health.'},
-  {trigger:'Secure and Encrypted Data',content:'Rest assured that your personal health data is protected with robust encryption, ensuring your privacy and security.'}
+  {title:'Personalized Logging Screen',content:'Organize the app the way it suits you best. Remove or add fields to suit your therapy needs.',image:featuresImages[0]},
+  {title:'RealTime Reading Of Glucose',content:'Get instant access to your glucose levels, ensuring that you are always in control of your health.',image:featuresImages[1]},
+  {title:'Chart Your Glucose Levels',content:'Visualize your glucose data through easy-to-understand charts, helping you identify patterns and trends.',image:featuresImages[2]},
+  {title:'Predict Glucose Levels',content:'Utilize advanced algorithms to anticipate future glucose levels, allowing you to proactively manage your health.',image:featuresImages[3]},
+  {title:'Secure and Encrypted Data',content:'Rest assured that your personal health data is protected with robust encryption, ensuring your privacy and security.',image:featuresImages[4]}
 ];
 
-const featuresImages = [
-  loginImg,abundance_dashboard,registerImg,placeholder1,placeholder2
-]
+
 
 
 // DYNAMIC, TO PULL FROM DB
@@ -39,63 +39,87 @@ const membershipTier = [
 
 export default function Home() {
   
-  const [featureImgIndex,setFeatureImgIndex] = useState(0);
-
-
-  const handleFeatureChange = (index:number)=>{
-    setFeatureImgIndex(index)
-  }
   
   return (
     <div className='flex flex-col'>
-        <div className='min-h-screen xl:h-[85vh] w-full  md:flex md:flex-col gap-3 relative
-        xl:flex-row justify-center items-center mt-8 p-2 mb-20'>
-            <div className="w-full md:w-[600px] md:h-[300px]   p-4 md:p-0   flex flex-col gap-1 md:gap-4  xl:text-start ">
-                <h1 className='text-4xl md:text-6xl font-bold  '>BE THE <span className='text-[#009797]'>CHANGE</span></h1>
-                <h2 className='text-4xl md:text-6xl font-bold'>YOU WANT <span className='text-[#009797]'>TO SEE</span></h2>
-                <p className='text-lg font-light tracking-wide my-4 xl:w-[90%]'>Welcom to Abundance, the calories and glucose management app made for people who want to take control of their health</p>
-                <p className='text-lg font-light '>Let us work together towards a brighter future!</p>
+    
+        <motion.div className='min-h-screen xl:h-[85vh] w-full  
+          md:flex flex-col gap-3
+         items-center pt-24 xl:pt-0 p-2 mb-20'
 
-                <div className='flex flex-col md:flex-row  mt-[4%] gap-2 md:gap-10 md:justify-center xl:justify-start'>
-                <Button className='py-6 px-8 text-[#009797] bg-white shadow-lg border-2 rounded-lg md:max-w-52'>
+         initial={{opacity:0, y: 50}}
+         whileInView={{opacity:1, y:0, transition:{delay: 0.2 ,duration: 0.5}}}
+         viewport={{once:false,amount:0.5}}
+         >
+
+        <img         
+        src={abundance_dashboard} className='w-full md:w-[70%] lg:w-1/2 xl:w-[35%] rounded-lg'></img>
+
+        <div className="w-full 
+                      md:w-[70%] lg:w-1/2 xl:w-[35%] p-4 md:p-0  my-8
+                        flex flex-col gap-1 md:gap-4 ">
+
+            <h1 className='text-4xl md:text-6xl font-bold  tracking-wide leading-relaxed'>BE THE <span className='text-[#00ACAC]'>CHANGE</span> YOU WANT TO <span className='text-[#00ACAC]'>SEE</span></h1>
+      
+            <p className='text-2xl  tracking-wide my-4 xl:w-[90%]'>Use Our App, eat healthy, and take control of your insulin levels</p>
+
+
+            <Button className='p-8 text-white bg-black shadow-lg font-bold rounded-lg '>
                   <div className='flex items-end gap-2'>
                    <h1>Download on IOS</h1>     
                   <FontAwesomeIcon icon={faApple} className='size-6' /> 
                   </div>
-               
                   </Button>
 
-                <Button className='py-6 px-8 bg-[#009797] text-white shadow-lg border-2 rounded-lg md:max-w-52'>
-                  <div className='flex items-center gap-3'>
-                   <h1>Download on Playstore</h1>     
-                   <FontAwesomeIcon icon={faGooglePlay} className='size-4' />
-                  </div>
-                  
-                   </Button>
-                </div>
-            </div>
+ 
 
-            <div className='h-[90%]  flex md:items-center justify-center md:mt-14'>
-            <img src={abundance_dashboard} className='object-fit h-[70%] md:h-[85%] lg:h-[90%] xl:h-[95%] mt-[5%] p-2 lg:mt-5'></img>
-            </div>
-      
+                  <Button className='p-8 text-[#00ACAC] bg-black shadow-lg font-bold rounded-lg '>
+                  <div className='flex items-end gap-2'>
+                   <h1>Download on Playstore</h1>     
+                    <FontAwesomeIcon icon={faGooglePlay} className='size-6' /> 
+                    </div>
+                  </Button>
+
+
         </div>
 
+ 
 
 
-      <div className='min-h-screen justify-center relative bg-[#EEEEEE] flex flex-col items-center  pb-10 '>
 
-          <h1 className='text-center text-5xl font-bold text-[#5D5D5D]  p-5  m-2 xl:p-14'>Explore <span className='text-[#009797]'>Abundance </span> App <span className='text-[#009797]'>Features</span></h1>
+        </motion.div>
+
+
+
+  
+          <motion.h1 
+          initial={{opacity:0, y: 80}}
+          whileInView={{opacity:1, y:0, transition:{delay: 0.2 ,duration: 0.4}}}
+          viewport={{once:false,amount:0.4}}
+          className='text-center text-5xl font-bold text-[#5D5D5D]  p-5  m-2 xl:p-14 h-[20vh]'>
+            Explore <span className='text-[#009797]'>Abundance </span> App <span className='text-[#009797]'>Features</span></motion.h1>
           
 
-          {/* FOR MEDIUM SCREENS ABOVE */}
-          <div className=' md:flex items-center justify-center '>
-          <div className='h-[90%]  flex flex-col md:flex-row md:items-center md:justify-center w-full gap-10 '>
-            <img src={featuresImages[featureImgIndex]} className='object-fit h-[70%] md:h-[85%] lg:h-[90%] xl:h-[95%] mt-[5%] p-2 lg:mt-5 '></img>
-            <CustomAccordion appFeatures={appFeatures} handleFeatureChange={handleFeatureChange}/>
-          </div>
-          </div>
-      </div>
+
+        {/* <motion.div className='h-[50vh]  w-full flex flex-col lg:flex-row items-center justify-center gap-10'
+             initial={{opacity:0, y: 80}}
+             whileInView={{opacity:1, y:0, transition:{delay: 0.2 ,duration: 0.5}}}
+             viewport={{once:false,amount:1}}>
+        <img src={feature1} className='w-96 rounded-xl h-96 '></img>
+
+        <div className='flex flex-col leading-loose tracking-wide gap-5 px-10 md:w-1/2'>
+        <h1 className="text-5xl">Personalized Logging Screen</h1>
+        <h2 className='font-light'>Organize the app the way it suits you best. Remove or add fields to suit your therapy needs. The order of the fields can be rearranged.</h2>
+        </div>
+
+        </motion.div> */}
+
+          {appFeatures.map((item,index)=>(
+            <MotionFeature imgSrc={item.image} title={item.title} content={item.content} key={index}/>
+          ))}
+
+
+
 
 
 
